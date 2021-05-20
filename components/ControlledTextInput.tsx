03267443a790ff49719aef5b8e23118c835ca2fd
Gabriel Pérez <gabriel@garox.org>
2021-05-20 11:21:16 -0400

    use real type

--- components/ControlledTextInput.tsx
+++ components/ControlledTextInput.tsx
@@ -2,17 +2,15 @@ import React from "react";
 import { Control, Path, useController } from "react-hook-form";
 import { TextInput, TextInputProps } from "react-native";
 
-type InputProps<T> = TextInputProps & {
+type ControlledTextInputProps<T> = TextInputProps & {
   name: Path<T>;
   control: Control<T>;
   defaultValue?: string;
 };
 
-type InputComponent<T> = React.PropsWithChildren<InputProps<T>>;
-
 export const ControlledTextInput: <T extends Record<string, unknown>>(
-  props: InputComponent<T>
-) => ReturnType<React.FC<InputComponent<T>>> = ({
+  props: ControlledTextInputProps<T>
+) => ReturnType<React.FC<ControlledTextInputProps<T>>> = ({
   name,
   control,
   defaultValue = "",
