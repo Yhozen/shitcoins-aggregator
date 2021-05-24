44dc8e3c65e61162a9a801cd9dbf9e0811ad9e64
camilodiaz37 <camilodiaz37@gmail.com>
2021-05-24 00:59:10 -0400

    add intro

--- App.tsx
+++ App.tsx
@@ -7,19 +7,26 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 import useCachedResources from "./hooks/useCachedResources";
 import useColorScheme from "./hooks/useColorScheme";
 import Navigation from "./navigation";
+import IntroSlider from "./components/tutorial/IntroSilder";
+import { useStore } from "./hooks/useStore";
 
 export default function App() {
+  const { isDoneIntro, setIsDoneIntro } = useStore();
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
 
   if (!isLoadingComplete) {
     return null;
   } else {
-    return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
-    );
+    if (isDoneIntro) {
+      return (
+        <SafeAreaProvider>
+          <Navigation colorScheme={colorScheme} />
+          <StatusBar />
+        </SafeAreaProvider>
+      );
+    } else {
+      return <IntroSlider isDone={isDoneIntro} setIsDone={setIsDoneIntro} />;
+    }
   }
 }
--- hooks/useStore.ts
+++ hooks/useStore.ts
@@ -1,12 +1,16 @@
-import create from "zustand"
-import  Web3 from "web3"
+import create from "zustand";
+import Web3 from "web3";
 
 type Store = {
-  web3: Web3
-}
+  web3: Web3;
+  isDoneIntro: boolean;
+  setIsDoneIntro: () => void;
+};
 
-const web3 = new Web3('https://bsc-dataseed1.binance.org:443');
+const web3 = new Web3("https://bsc-dataseed1.binance.org:443");
 
-export const useStore = create<Store>(set => ({
+export const useStore = create<Store>((set) => ({
   web3: web3,
-}))
+  isDoneIntro: false,
+  setIsDoneIntro: () => set((state) => ({ isDoneIntro: true })),
+}));
