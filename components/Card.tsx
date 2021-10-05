6713d93f12e6f5a4053ffb493a408aafe6768dce
Gabriel P??rez <gabriel@garox.org>
2021-10-05 15:57:43 -0300

    giving up

--- components/Card.tsx
+++ components/Card.tsx
@@ -2,78 +2,79 @@ import React from "react";
 
 import tailwind from "tailwind-rn";
 import styled from "styled-components/native";
+import ShadowView from "../rokket-components/mixins/shadow/md";
 
 const CardContainer = styled.View`
-  ${tailwind("flex flex-wrap place-items-center h-screen")}
+  ${tailwind("flex flex-wrap place-items-center h-screen")};
 `;
 
 const CardInnerContainer = styled.View`
   ${tailwind(
     "overflow-hidden shadow-lg transition duration-500 ease-in-out transform hover:-translate-y-5 hover:shadow-2xl rounded-lg h-90 w-60 md:w-80 cursor-pointer m-auto"
-  )}
+  )};
 `;
 
 const CardPressable = styled.View`
-  ${tailwind("w-full block h-full")}
+  ${tailwind("w-full block h-full")};
 `;
 
 const ImageCover = styled.Image`
-  ${tailwind("max-h-40 w-10 h-40 w-full object-cover")}
+  ${tailwind("max-h-40 w-10 h-40 w-full object-cover")};
 `;
 
 const ProfileImage = styled.Image`
-  ${tailwind("w-10 h-10 object-cover rounded-full")}
+  ${tailwind("w-10 h-10 object-cover rounded-full")};
 `;
 
 const Content = styled.View`
-  ${tailwind("bg-white w-full p-4")}
+  ${tailwind("bg-white w-full p-4")};
 `;
 
 const Title = styled.Text`
-  ${tailwind("text-indigo-500 text-2xl font-medium")}
+  ${tailwind("text-indigo-500 text-2xl font-medium")};
 `;
 
 const Subtitle = styled.Text`
-  ${tailwind("text-gray-800 text-sm font-medium mb-2")}
+  ${tailwind("text-gray-800 text-sm font-medium mb-2")};
 `;
 
 const MainText = styled.Text`
-  ${tailwind("text-gray-600 font-light text-md")}
+  ${tailwind("text-gray-600 font-light text-md")};
 `;
 
 const TextPressable = styled.Text`
-  ${tailwind("inline-flex text-indigo-500")}
+  ${tailwind("inline-flex text-indigo-500")};
 `;
 
 const Tag = styled.Text`
-  ${tailwind("m-1 px-2 py-1 rounded bg-indigo-500 text-white")}
+  ${tailwind("m-1 px-2 py-1 rounded bg-indigo-500 text-white")};
 `;
 
 const TagsContainer = styled.View`
   ${tailwind(
     "flex flex-wrap justify-starts items-center py-3 border-gray-200 border-b-2 text-xs text-white font-medium"
-  )}
+  )};
 `;
 
 const ProfileContainer = styled.View`
-  ${tailwind("flex items-center mt-2")}
+  ${tailwind("flex items-center mt-2")};
 `;
 
 const ProfileContent = styled.View`
-  ${tailwind("pl-3")}
+  ${tailwind("pl-3")};
 `;
 
 const ProfileName = styled.Text`
-  ${tailwind("font-medium")}
+  ${tailwind("font-medium")};
 `;
 
 const ProfileRole = styled.Text`
-  ${tailwind("text-gray-600 text-sm")}
+  ${tailwind("text-gray-600 text-sm")};
 `;
 
 const Card = () => {
   return (
-    <CardContainer>
+    <ShadowView>
       <CardInnerContainer>
         <CardPressable>
           <ImageCover
@@ -111,7 +112,7 @@ const Card = () => {
           </Content>
         </CardPressable>
       </CardInnerContainer>
-    </CardContainer>
+    </ShadowView>
   );
 };
 
